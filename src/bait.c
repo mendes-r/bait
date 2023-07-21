9f597b8357c626259186009213aaae72642d883c
Ricardo Mendes <mendes.r@gmx.com>
2023-07-21 21:38:27 +0100

    Implement main logic

--- src/bait.c
+++ src/bait.c
@@ -1,8 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define DEBUG "Debug: "
+#define BUFSIZE 10
+
+void help_page(void);
+void catch(void);
+void release(void);
+void grab(void);
 
 void bait(char *cmd){
-  printf("%s", cmd);
+  printf("%s%s\n", DEBUG, cmd);
+
+  if (strcmp(cmd, "catch") == 0 || strcmp(cmd, "") == 0){
+    catch();
+    printf("catch\n");
+  } else if (strcmp(cmd, "release") == 0 || strcmp(cmd, "-r") == 0){
+    release();
+    printf("release\n");
+  } else if (strcmp(cmd, "grab") == 0 || strcmp(cmd, "-g") == 0) {
+    grab();
+    printf("grab\n");
+  } else {
+    help_page();
+  }
+
 }
 
 void catch(void){
@@ -32,17 +55,18 @@ void help_page(void){
 }
 
 int main(int argc, char *argv[]){
+  char *cmd = ""; 
 
-  // skip program name
-  --argc;
-  ++argv;
+  if (argc > 1) {
+    cmd = argv[1];
+  }
 
-  // no arguments or empty string
-  if (argc < 1 || argv == 0) {
-    help_page();
-    exit(1);
+  printf("%s%s\n", DEBUG, cmd);
+
+  if (strlen(cmd) > BUFSIZE) {
+    cmd = "help";
   }
  
-  bait(argv[0]);
+  bait(cmd);
   return EXIT_SUCCESS;
 }
